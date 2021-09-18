f6200ec240088faa36b3e9ce04b51cf370481fbe
Daniel Baum <dmbaum@gmail.com>
2021-09-18 09:47:47 +0300

    Uncommented LED easter egg.

--- UK101/UK101/uk101.vhd
+++ UK101/UK101/uk101.vhd
@@ -103,7 +103,7 @@ architecture struct of uk101 is
 
 begin
 
-   --led <= latchedbits(0);
+   led <= latchedbits(0);
 
 	serialClkCount1 <= c_9600BaudClkCount1 when baud_rate = '0' else c_300BaudClkCount1;
 	serialClkCount2 <= c_9600BaudClkCount2 when baud_rate = '0' else c_300BaudClkCount2;
@@ -113,8 +113,8 @@ begin
 	n_dispRamCS <= '0' when cpuAddress(15 downto 10) = "110100" else '1';
 	n_basRomCS <= '0' when cpuAddress(15 downto 13) = "101" else '1'; --8k
 	n_monitorRomCS <= '0' when cpuAddress(15 downto 11) = "11111" else '1'; --2K
-	--n_iocs <= '0' when cpuAddress(15 downto 0 ) = "1101010000000000" else '1'; 
-	n_ramCS <= not(n_dispRamCS and n_basRomCS and n_monitorRomCS and n_aciaCS and n_kbCS);
+	n_iocs <= '0' when cpuAddress(15 downto 0 ) = "1101010000000000" else '1'; 
+	n_ramCS <= not(n_dispRamCS and n_basRomCS and n_monitorRomCS and n_aciaCS and n_kbCS and n_iocs);
 	n_aciaCS <= '0' when cpuAddress(15 downto 1) = "111100000000000" else '1';
 	n_kbCS <= '0' when cpuAddress(15 downto 10) = "110111" else '1';
  
@@ -297,14 +297,14 @@ begin
 		KEYB	=> kbReadData
 	);
 	
---	UserLed	:	entity work.OutLatch
---	port map (
---		dataIn	=> cpuDataOut,
---		clock		=> clk,
---		load		=> n_iocs,
---		clear		=> n_reset,
---		latchOut	=> latchedbits
---	);
+	UserLed	:	entity work.OutLatch
+	port map (
+		dataIn	=> cpuDataOut,
+		clock		=> clk,
+		load		=> n_iocs,
+		clear		=> n_reset,
+		latchOut	=> latchedbits
+	);
 --	
 	process (n_kbCS,n_memWR)
 	begin
