720c0147d36a16f5d1f07a78351aceae857b5622
Daniel Baum <dmbaum@gmail.com>
2021-10-01 01:09:11 +0300

    Increased text buffer size to 65535. Small attempted bugfixes.

--- UK101/Components/UART/bufferedUART.vhd
+++ UK101/Components/UART/bufferedUART.vhd
@@ -49,13 +49,13 @@ end bufferedUART;
 
 architecture rtl of bufferedUART is
 	
-	type byteArray is array (0 to 32767) of std_logic_vector(7 downto 0);
+	type byteArray is array (0 to 65535) of std_logic_vector(7 downto 0);
 	signal ascii_data : byteArray;
 	signal ascii : std_logic_vector(7 downto 0);
    signal in_dl : std_logic;
 	signal ascii_rdy : std_logic;
 	signal w_data_ready : std_logic;
-	signal i_outCounter  : integer range 0 to 32767 := 0;
+	signal i_outCounter  : integer range 0 to 65535 := 0;
 	signal i_ascii_last_byte : integer range 0 to 65535 := 0;
 	signal i_ioctl_addr : integer range 0 to 65535 := 0;
 	signal prev_clk : std_logic;
@@ -101,9 +101,9 @@ begin
 
 		i_ioctl_addr <= to_integer(unsigned(ioctl_addr));
 		dataOut <= fileOut when loadFrom = '0' else uartOut;
-		statusReg(0) <= '0' when loadFrom = '0' and i_ioctl_addr = i_outCounter 
+		statusReg(0) <= '0' when loadFrom = '0' and i_ioctl_addr = i_outCounter
 							else '0' when loadFrom = '1' and rxInPointer=rxReadPointer else '1';
-		statusReg(1) <=  '0' when loadFrom = '0' else
+		statusReg(1) <=  '1' when loadFrom = '0' else
 						     '1' when loadFrom = '1' and txByteWritten=txByteSent else '0';
 		statusReg(2) <= n_dcd;
 		statusReg(3) <= n_cts;
@@ -111,12 +111,12 @@ begin
 		  
 		-- interrupt mask
 		n_int <= n_int_internal;
-		n_int_internal <= '0' when loadFrom = '0' and (i_ioctl_addr /= i_outCounter) and in_dl = '1' 
+		n_int_internal <= '0' when loadFrom = '0' and (i_ioctl_addr /= i_outCounter) and in_dl ='1'
 				else '0' when loadFrom = '1' and (rxInPointer /= rxReadPointer) and controlReg(7)='1'
 	         else '0' when loadFrom = '1' and (txByteWritten=txByteSent) and controlReg(6)='0' and controlReg(5)='1'
 				else '1';
 	
-	    w_data_ready <= in_dl and  (not ioctl_download);
+	    w_data_ready <= (in_dl and (not ioctl_download)) when loadFrom = '0' else '1';
 		 data_ready <= w_data_ready;
 		 
 		 -- raise (inhibit) n_rts when buffer over half-full
@@ -153,7 +153,7 @@ begin
 						i_outCounter <= 0;
 						in_dl <= '1';
 					else
-						if in_dl = '1' and i_outCounter > i_ioctl_addr then
+						if in_dl = '1' and i_outCounter = i_ioctl_addr then
 							in_dl<= '0';
 						end if;
 					end if;
