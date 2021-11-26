e66e2fc4dadfa2c8060dbb1cc8c757d7e92b5e96
Daniel Baum <dmbaum@gmail.com>
2021-11-26 10:57:00 +0200

    Remove parameterised bit width from outlatch.

--- UK101/UK101/outlatch.vhd
+++ UK101/UK101/outlatch.vhd
@@ -9,13 +9,12 @@ use ieee.std_logic_unsigned.all;
 ---------------------------------------------------
 
 entity OutLatch is
-generic(n: natural :=8);
 	port(	
-		dataIn	:	in std_logic_vector(n-1 downto 0);
+		dataIn	:	in std_logic_vector(7 downto 0);
 		clock		:	in std_logic;
 		load		:	in std_logic;
 		clear		:	in std_logic;
-		latchOut	:	out std_logic_vector(n-1 downto 0)
+		latchOut	:	out std_logic_vector(7 downto 0)
 	);
 end OutLatch;
 
@@ -23,14 +22,13 @@ end OutLatch;
 
 architecture behv of OutLatch is
 
-    signal Q_tmp: std_logic_vector(n-1 downto 0);
+    signal Q_tmp: std_logic_vector(7 downto 0);
 
 begin
 	process(clock, load, clear)
 	begin
 		if clear = '0' then
-		-- use 'range in signal assigment 
-			Q_tmp <= (Q_tmp'range => '0');
+			Q_tmp <= X"01";
 		elsif rising_edge(clock) then
 			if load = '0' then
 				Q_tmp <= dataIn;
